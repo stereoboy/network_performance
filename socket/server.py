da4652c299835f53a6b00c6d157d3e5c6ee2ac3d
Jaeyoung Lee <jylee0121@gmail.com>
2019-07-22 13:31:21 +0900

    update: socket server port
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- socket/server.py
+++ socket/server.py
@@ -13,7 +13,7 @@ print("Socket successfully created")
 
 # reserve a port on your computer in our 
 # case it is 12345 but it can be anything 
-port = 12345                
+port = 9090                
 
 # Next bind to the port 
 # we have not typed any ip in the ip field 
