9f27e87123bec948d5bb30cdb6755002b4d8f28a
Jaeyoung Lee <jylee0121@gmail.com>
2019-07-22 13:29:30 +0900

    minor fix
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- socket/server.py
+++ socket/server.py
@@ -9,7 +9,7 @@ import numpy as np
 
 # next create a socket object 
 s = socket.socket()          
-print "Socket successfully created"
+print("Socket successfully created")
 
 # reserve a port on your computer in our 
 # case it is 12345 but it can be anything 
@@ -21,11 +21,11 @@ port = 12345
 # this makes the server listen to requests  
 # coming from other computers on the network 
 s.bind(('', port))         
-print "socket binded to %s" %(port) 
+print("socket binded to %s" %(port))
 
 # put the socket into listening mode 
 s.listen(5)      
-print "socket is listening"            
+print("socket is listening")
 
 # a forever loop until we interrupt it or  
 # an error occurs 
@@ -33,7 +33,7 @@ while True:
 
     # Establish connection with client. 
     c, addr = s.accept()      
-    print 'Got connection from', addr 
+    print('Got connection from', addr)
 
     data_size = 640*480*4
 
