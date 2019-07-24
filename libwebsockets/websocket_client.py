903eae2a46368aa95e7d535b13c941d3c63a9aca
Jaeyoung Lee <jylee0121@gmail.com>
2019-07-24 21:04:47 +0900

    update: websocket option for remote
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- libwebsockets/websocket_client.py
+++ libwebsockets/websocket_client.py
@@ -6,11 +6,22 @@ import numpy as np
 import time
 import collections
 import cv2
+import argparse
+import sys
 
 def main():
+
+    parser = argparse.ArgumentParser(description='This is toy client to test comm latency')
+    parser.add_argument('-r', '--remote', action='store_true', default=False)
+    options = parser.parse_args(sys.argv[1:])
+    print(options)
+
     img = cv2.imread('../image.png', cv2.IMREAD_UNCHANGED)
     print('img.shape={}'.format(img.shape))
-    ws = create_connection("ws://127.0.0.1:9090")
+    if options.remote:
+        ws = create_connection("ws://192.168.0.224:9090")
+    else:
+        ws = create_connection("ws://127.0.0.1:9090")
     elapsed_time_queue = collections.deque(maxlen=100)
     start = time.time()
     count = 0
@@ -26,7 +37,7 @@ def main():
         count += 1
         if count%100 == 0:
             print("{} fps".format(1.0/np.mean(elapsed_time_queue)))
-        
+
     ws.close()
 
 if __name__ == "__main__":
