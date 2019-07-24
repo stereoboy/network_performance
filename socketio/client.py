f7cc2bdf473059259bb35d4044902a0ddff94e9e
Jaeyoung Lee <jylee0121@gmail.com>
2019-07-24 17:27:01 +0900

    update: implement socketio client/server
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- socketio/client.py
+++ socketio/client.py
@@ -1,18 +1,25 @@
+'''
+    reference: https://stackoverflow.com/questions/54703656/python-socketio-how-to-emit-message-from-server-to-client
+'''
+
 import socketio
 import numpy as np
 import time
 import collections
+import queue
+import cv2
 
+q = queue.Queue()
 sio = socketio.Client()
 
 @sio.on('connect')
 def connect():
     print('connection established')
 
-@sio.on('message')
+@sio.on('ch1')
 def message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+    #print('>>> message received with ', data)
+    q.put(data)
 
 @sio.on('disconnect')
 def disconnect():
@@ -21,18 +28,18 @@ def disconnect():
 if __name__ == '__main__':
 
     sio.connect('http://localhost:5000')
+    #sio.connect('http://192.168.0.224:5000')
     elapsed_time_queue = collections.deque(maxlen=100)
     start = time.time()
     count = 0
 
-    while True:
-        data = np.zeros((480, 640, 4), dtype=np.uint8).tostring()
-        #print(len(data))
-        sio.emit(data)
-#        print("Receiving...")
-#        result =  ws.recv()
-#        print("Received '%s'" % result)
+    img = cv2.imread('../image.png', cv2.IMREAD_UNCHANGED)
 
+    while True:
+        data = img.tostring()
+        #print(len(data))
+        sio.emit('ch1', data)
+        q.get()
         end = time.time()
         elapsed = end - start
         start = time.time()
--- socketio/server.py
+++ socketio/server.py
@@ -1,5 +1,7 @@
 import eventlet
 import socketio
+import numpy as np
+import cv2
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
@@ -8,15 +10,19 @@ app = socketio.WSGIApp(sio, static_files={
 
 @sio.on('connect')
 def connect(sid, environ):
-    print('connect ', sid)
+    print('>>> connect ', sid)
 
-@sio.on('message')
-def message(sid, data):
-    print('message ', data)
+@sio.on('ch1')
+def on_message(sid, data):
+    print('>>> \tmessage')
+    img = np.frombuffer(data, dtype=np.uint8).reshape((480, 640, 4))
+    sio.emit('ch1', b'\x00')
+#    cv2.imshow('display', img)
+#    cv2.waitKey(1)
 
 @sio.on('disconnect')
 def disconnect(sid):
-    print('disconnect ', sid)
+    print('>>> disconnect ', sid)
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
