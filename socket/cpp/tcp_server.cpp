c6bc266ae89e25258ae04f4295ae18a9dab56070
Jaeyoung Lee <jylee0121@gmail.com>
2022-09-14 10:39:06 +0900

    update: client ip identification
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- socket/cpp/tcp_server.cpp
+++ socket/cpp/tcp_server.cpp
@@ -10,13 +10,13 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #define MAX 80
 #define PORT 8080
-#define SA struct sockaddr
-   
+
 // Function designed for chat between client and server.
 void func(int connfd)
 {
@@ -25,7 +25,7 @@ void func(int connfd)
     // infinite loop for chat
     for (;;) {
         bzero(buff, MAX);
-   
+
         // read the message from client and copy it in buffer
         read(connfd, buff, sizeof(buff));
         // print buffer which contains the client contents
@@ -35,10 +35,10 @@ void func(int connfd)
         // copy server message in the buffer
         while ((buff[n++] = getchar()) != '\n')
             ;
-   
+
         // and send that buffer to client
         write(connfd, buff, sizeof(buff));
-   
+
         // if msg contains "Exit" then server exit and chat ended.
         if (strncmp("exit", buff, 4) == 0) {
             printf("Server Exit...\n");
@@ -46,14 +46,14 @@ void func(int connfd)
         }
     }
 }
-   
+
 // Driver function
 int main()
 {
     int sockfd, connfd;
     socklen_t len;
     struct sockaddr_in servaddr, cli;
-   
+
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
@@ -63,20 +63,20 @@ int main()
     else
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
-   
+
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
-   
+
     // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+    if ((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
         printf("socket bind failed...\n");
         exit(0);
     }
     else
         printf("Socket successfully binded..\n");
-   
+
     // Now server is ready to listen and verification
     if ((listen(sockfd, 5)) != 0) {
         printf("Listen failed...\n");
@@ -85,19 +85,37 @@ int main()
     else
         printf("Server listening..\n");
     len = sizeof(cli);
-   
+
     // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
+    connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
     if (connfd < 0) {
         printf("server accept failed...\n");
         exit(0);
     }
     else
         printf("server accept the client...\n");
-   
+
+
+    // reference
+    //   * https://stackoverflow.com/questions/9590529/how-should-i-print-server-address
+    //   * http://man7.org/linux/man-pages/man3/inet_ntop.3.html
+    char ip_str[INET_ADDRSTRLEN] = {};
+    inet_ntop(AF_INET, &cli.sin_addr.s_addr, ip_str, sizeof(INET_ADDRSTRLEN));
+    printf("  accepted from (addr=%s, port=%d)\n", ip_str, ntohs(cli.sin_port)) ;
+
+    // reference
+    //   * https://stackoverflow.com/questions/3060950/how-to-get-ip-address-from-sock-structure-in-c
+    struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&cli;
+    struct in_addr ipAddr = pV4Addr->sin_addr;
+
+    char str[INET_ADDRSTRLEN];
+    inet_ntop( AF_INET, &ipAddr, str, INET_ADDRSTRLEN );
+    printf("  accepted from (addr=%s, port=%d)\n", str, ntohs(pV4Addr->sin_port)) ;
+
+
     // Function for chatting between client and server
     func(connfd);
-   
+
     // After chatting close the socket
     close(sockfd);
 }
