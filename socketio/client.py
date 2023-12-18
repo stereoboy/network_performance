2a17b58f891dcb42023f9a78a53cb580d42bc72b
Jaeyoung Lee <jylee0121@gmail.com>
2023-12-18 16:57:47 +0900

    update: update socketio python sample
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- socketio/client.py
+++ socketio/client.py
@@ -10,45 +10,52 @@ import queue
 import argparse
 import sys
 import cv2
+import config
+import logging
+
+logger = logging.getLogger('Socketio.Client ')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(fmt='%(asctime)s:[%(levelname)s][%(process)d][%(name)s] - %(funcName)s:%(lineno)d - %(message)s'))
+logger.addHandler(handler)
 
 q = queue.Queue()
 sio = socketio.Client()
 
 @sio.on('connect')
 def connect():
-    print('connection established')
+    logger.info('connection established')
 
 @sio.on('ch1')
 def message(data):
-    #print('>>> message received with ', data)
+    logger.debug('>>> message received with ', data)
     q.put(data)
 
 @sio.on('disconnect')
 def disconnect():
-    print('disconnected from server')
+    logger.info('disconnected from server')
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='This is toy client to test comm latency')
-    parser.add_argument('-r', '--remote', action='store_true', default=False)
-    options = parser.parse_args(sys.argv[1:])
-    print(options)
+    parser.add_argument('-H', '--host', action='store', type=str, default='http://localhost')
+    parser.add_argument('-p', '--port', action='store', type=int, default=config.PORT)
 
-    if options.remote:
-        sio.connect('http://192.168.0.224:9090', transports='polling')
-    else:
-        #sio.connect('http://localhost:9090')
-        sio.connect('http://127.0.0.1:9090', transports='polling')
+    options = parser.parse_args(sys.argv[1:])
+    logger.info(options)
+
+
+    sio.connect('{}:{}'.format(options.host, options.port), transports='polling')
     elapsed_time_queue = collections.deque(maxlen=100)
     start = time.time()
     count = 0
 
     #img = cv2.imread('../image.png', cv2.IMREAD_UNCHANGED)
     img = np.zeros((480, 640, 4), dtype=np.uint8)
-    print("sample image shape = {}".format(img.shape))
+    logger.info("sample image shape = {}".format(img.shape))
 
     while True:
-        data = img.tostring()
-        #print(len(data))
+        data = img.tobytes()
+        logger.debug(len(data))
         sio.emit('ch1', bytes(data))
         q.get()
         end = time.time()
@@ -57,7 +64,7 @@ if __name__ == '__main__':
         elapsed_time_queue.append(elapsed)
         count += 1
         if count%100 == 0:
-            print("{} fps".format(1.0/np.mean(elapsed_time_queue)))
+            logger.info("\t{} fps".format(1.0/np.mean(elapsed_time_queue)))
 
-
-
+if __name__ == '__main__':
+    main()
--- socketio/server.py
+++ socketio/server.py
@@ -4,8 +4,19 @@ import numpy as np
 import cv2
 import time
 import collections
+import config
+import logging
 
-sio = socketio.Server()
+logger = logging.getLogger('Socketio.Server')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(fmt='%(asctime)s:[%(levelname)s][%(process)d][%(name)s] - %(funcName)s:%(lineno)d - %(message)s'))
+logger.addHandler(handler)
+# logging.basicConfig(level=logging.WARNING, format='%(asctime)s:[%(levelname)s][%(process)d][%(name)s] - %(funcName)s:%(lineno)d - %(message)s')
+
+
+# sio = socketio.Server(logger=logger, engineio_logger=True, max_http_buffer_size=480*640*4*2)
+sio = socketio.Server(logger=False, engineio_logger=False, max_http_buffer_size=480*640*4*2)
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
@@ -13,7 +24,10 @@ app = socketio.WSGIApp(sio, static_files={
 @sio.on('connect')
 def connect(sid, environ):
     global count, elapsed_time_queue, start
-    print('>>> connect ', sid)
+    logger.info('>>> connect {}'.format(sid))
+    for k, v in environ.items():
+        logger.info('\t{}:{}'.format(k, v))
+
     count = 0
     elapsed_time_queue = collections.deque(maxlen=100)
     start = time.time()
@@ -21,7 +35,7 @@ def connect(sid, environ):
 @sio.on('ch1')
 def on_message(sid, data):
     global count, elapsed_time_queue, start
-    #print('>>> \tmessage')
+    logger.debug('>>> \tmessage: len(data)={}'.format(len(data)))
     #img = np.frombuffer(data, dtype=np.uint8).reshape((480, 640, 4))
     end = time.time()
     elapsed = end - start
@@ -29,14 +43,15 @@ def on_message(sid, data):
     elapsed_time_queue.append(elapsed)
     count += 1
     if count%100 == 0:
-        print("{} fps".format(1.0/np.mean(elapsed_time_queue)))
-    sio.emit('ch1', b'\x00')
+        logger.info("\t{} fps".format(1.0/np.mean(elapsed_time_queue)))
+    sio.emit('ch1', b'\x00', room=sid)
 #    cv2.imshow('display', img)
 #    cv2.waitKey(1)
 
 @sio.on('disconnect')
 def disconnect(sid):
-    print('>>> disconnect ', sid)
+    logger.info('>>> disconnect ', sid)
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 9090)), app)
+    logger.info('python-socket.io server')
+    eventlet.wsgi.server(eventlet.listen(('', config.PORT)), app)
